d2f087874b7df8c6a10351e3e1469001cd46d73a
Olivia Beck <olb5053@psu.edu>
2024-02-21 13:48:35 -0500

    added comments to check github build

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,7 @@
 #' My Hello World Function
 #'
+#' Adding comments to see if the website updates too
+#'
 #' @param x The name of the person to say hi to
 #'
 #' @return The output from \code{\link{print}}
